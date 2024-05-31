d69e7f7fcac9259b00296c22c598744956295a1e
Michael Pini <michael.roland.pini@outlook.com>
2024-06-01 00:02:08 +0200

    ---

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { LoggingService } from './shared/logging.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: []  
 })
 export class AppComponent {
   title = 'my-recepies';
+
+  constructor(private loggingService: LoggingService) { }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping/shopping-list-edit/shopping-list-edit.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { CrapComponent } from './crap/crap.component';
+import { CrapChildComponent } from './crap/crap-child/crap-child-component';
+import { HighlightHover } from './directives/highlight.directive';
+import { IfNotDirective } from './directives/if-not.directive';
+import { HideIfDirective } from './directives/hide-if.directive';
+import { DropdownDirective } from './directives/dropdown.directive';
+import { ToggleClassDirective } from './directives/toggle-class.directive';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    CrapComponent,
+    CrapChildComponent,
+    HighlightHover,
+    IfNotDirective,
+    HideIfDirective,
+    DropdownDirective,
+    ToggleClassDirective,
+    RecipeEditComponent,
+    CrapChildComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
